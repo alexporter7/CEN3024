ecab8450232980597e1fb432523684681090fcb0
Alex <53269726+alexporter7@users.noreply.github.com>
2020-08-27 15:05:34 -0400

    Got proof of conept for Ch3

--- chapter3/chp3_text_analyzer.java
+++ chapter3/chp3_text_analyzer.java
@@ -3,3 +3,37 @@
 //Program Name: chp3_text_analyzer
 //Purpose: Download play, process data, return word frequency
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
+
+class chp3_text_analyzer {
+
+    public static void main(String[] args) throws IOException {
+
+        //====================================================
+        //BELOW IS A PROOF OF CONCEPT
+        //I Have not used JSoup in a VERY long time
+        //Glad I could get something working so far!
+        //====================================================
+        // System.out.println("Test");
+
+
+        // String url = "http://shakespeare.mit.edu/macbeth/full.html";
+        // Document doc = Jsoup.connect(url).get();
+        
+
+        // String text = doc.select("blockquote").text();
+        // int length = text.length();
+        // text += doc.select("a").text();
+        // System.out.println(text);
+        // System.out.println("Before: " + length);
+        // System.out.println(text.length());
+        //====================================================
+        
+    }
+
+}
