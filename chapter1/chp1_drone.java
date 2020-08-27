1af3ab0cc4cbefe7bfe58ffc8bdeee3d23904c97
Alex <53269726+alexporter7@users.noreply.github.com>
2020-08-27 14:17:30 -0400

    Added Program Details
    Added Author/Date/Program Name/Purpose

--- chapter1/chp1_drone.java
+++ chapter1/chp1_drone.java
@@ -1,3 +1,8 @@
+//Author Name: Alex Porter
+//Date: 8/27/2020
+//Program Name: chp1_drone
+//Purpose: Create a CLI based drone application to track movements
+
 import java.io.IOException;
 import java.util.Scanner;
 
